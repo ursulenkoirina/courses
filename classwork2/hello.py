f75fc6036d56f7f579e424d24f4d2f77db17e0f4
ursulenkoirina <ursulenko_irina@ukr.net>
2019-02-14 22:49:49 +0200

    Added classwork2

---
+++ classwork2/hello.py
@@ -0,0 +1,21 @@
+# a = int(input("Enter number: "))
+# # print((type(a)))
+# if a > 0:
+#     print("hello")
+#     print(a)
+# else:
+#     print("no!!")
+
+#Task1
+# a = input("Input world: ")
+# if a == 'Hi' :
+#     print("hello")
+# print("Goobye")
+
+#Task2
+a = int(input("Input number: "))
+if 0<a<=100:
+    print("Correct")
+else:
+    print("incorec")
+
