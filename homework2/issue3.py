b9fb822bc7baf887055041e4eb993a2690237833
ursulenkoirina <ursulenko_irina@ukr.net>
2019-02-16 19:12:08 +0200

    remove homework2

--- homework2/issue3.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Input year: "))
-if (a % 4 == 0 and a % 100 != 0) or (a % 400 == 0):
-    print("Yes")
-else:
-    print("No")
--- homework2/issue7.py
+++
@@ -1,6 +0,0 @@
-d = {'name': 'irina', 'age': 27, 'profession': 'engineer', 'city': 'kiev'}
-d['age'] = d.get('age')+1
-d['profession'] = 'doctor'
-print(d)
-print(d.values())
-print(d.keys())
