a9bd637946e2483fc622cbb1223a0153501dce03
ursulenkoirina <ursulenko_irina@ukr.net>
2019-02-18 21:42:48 +0200

    Added issue3_1

---
+++ homework3/Issue3_1.py
@@ -0,0 +1,21 @@
+a = input("Input something: ")
+# 1.1
+print('1:', a[2], end='\n')
+# 1.2
+print('2: ', a[-2], end='\n')
+# 1.3
+print('3: ', a[:5], end='\n')
+# 1.4
+print('4:', a[:-2], end='\n')
+# 1.5
+print('5:', a[2::2], end='\n')
+# 1.6
+print('6:', a[1::2], end='\n')
+# 1.7
+print('7:', a[::-1], end='\n')
+# 1.8
+print('8: ', a[::-1], end='\n')
+# 1.9
+print('9: ', a[-2:0:-1], end='\n')
+# 1.10
+print('10: ', len(a), end='\n')
