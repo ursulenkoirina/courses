8b51767835796cb9c4f3e811f531477e0bea9cf3
ursulenkoirina <ursulenko_irina@ukr.net>
2019-02-23 13:30:11 +0200

    Fix: fixed issue3_1 and issue3_7

--- homework3/issue3_7.py
+++ homework3/issue3_7.py
@@ -1,28 +1,23 @@
+
 a = """
-We are not what we should be! We are not what we need to be. But at least we are not what we used to be (Football Coach)
+We are not what we should be!
+We are not what we need to be.
+But at least we are not what we used to be
+ (Football Coach)
 """
 # 7.1 Посчитайте сколько слов в тексте (разбейте на слова методом строк split)
-line_list = a.split(' ')
-print(line_list)
-print(len(line_list))
+line_list = a.split()
+print('Number of words: ', len(line_list))
 # 7.2 Удалите знаки препинания (пройдитесь циклом все слова и удалите методом strip знаки препинания)
 l = []
 for i in line_list:
-    if '(' in i:
-        i = i.strip('(')
-    if '\n' in i:
-        i = i.strip('\n')
-    if '!' in i:
-        i = i.strip('!')
-    if '.' in i:
-        i = i.strip('.')
-    if ')' in i:
-        i = i.strip(')')
+    if '(' in i or ')' in i or '.' in i or '!' in i:
+        i = i.strip('().!')
     l.append(i)
-print(l)
-#7.3 Выведите слова в алфавитном порядке (найдите метод списка, который сортирует)
+print('List without punctuation marks: \n', l)
+# 7.3 Выведите слова в алфавитном порядке (найдите метод списка, который сортирует)
 sort_list = sorted(l)
-print(sort_list)
+print('Sorted list: \n', sort_list)
 #7.4
 """
 Посчитать, сколько раз встречается каждое слово.
@@ -34,5 +29,4 @@ value_dict = {}
 for i in sort_list:
     j = sort_list.count(i)
     value_dict[i] = j
-print('My dict is : \n', value_dict)
-
+    print(value_dict)
